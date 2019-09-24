fe19399f1c52d5e2e1323883424b3cf0c16f618c
Ryan Brazeal <ryan.brazeal@ufl.edu>
2019-09-23 20:28:45 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -69,7 +69,7 @@ function draw(){
     
       //var hue = globalHue + (theta / Math.PI) * 180;
         var hue = 200;
-      ctx.strokeStyle = "hsl("+ hue +", 20%, 20%)";
+      ctx.strokeStyle = "hsl("+ hue +", 10%, 10%)";
       //ctx.lineWidth=clamp(map(r, -200, 200, 0.25, 2), 0.25, 10);
       ctx.lineWidth = .5;
       ctx.stroke();
